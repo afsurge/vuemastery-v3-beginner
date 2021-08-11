bef10dac517969f6fe85e033e2c0312bd02c0d97
Abrar Faisal <abrarfaisal20@gmail.com>
2021-08-11 18:28:20 +0200

    Computed Properties

--- intro-to-vue3/main.js
+++ intro-to-vue3/main.js
@@ -3,19 +3,23 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: "Socks",
-            image: "./assets/images/socks_blue.jpg",
-            inStock: true,
+            brand: "Vue Mastery",
+            // image: "./assets/images/socks_blue.jpg",
+            // inStock: true,
+            selectedVariant: 0,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
                 {
                     id: 2234,
                     color: "green",
                     image: "./assets/images/socks_green.jpg",
+                    quantity: 0,
                 },
                 {
                     id: 2235,
                     color: "blue",
                     image: "./assets/images/socks_blue.jpg",
+                    quantity: 50,
                 },
             ],
         };
@@ -24,8 +28,20 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
+            // console.log(index);
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
         },
     },
 });
