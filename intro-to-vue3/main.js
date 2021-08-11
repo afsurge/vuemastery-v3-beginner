07534ce7fe1d1213bdbd9e05e8842f3813a41d95
Abrar Faisal <abrarfaisal20@gmail.com>
2021-08-11 17:29:18 +0200

    Attribute binding

--- intro-to-vue3/main.js
+++ intro-to-vue3/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Socks",
-            description: "Nice!",
+            image: "./assets/images/socks_blue.jpg",
         };
     },
 });
